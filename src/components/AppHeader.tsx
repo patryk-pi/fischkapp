ccf1ac301a1936098059634b25885b7ac33e541c
patryk-pi <patryk.piasecki1@gmail.com>
2023-09-21 18:32:57 +0200

    added add button to header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -34,17 +34,27 @@ export const AppHeader = ({ cardsAmount = 0 }: AppHeaderProps) => {
                         <h1>Cards: {cardsAmount}</h1>
                     </div>
                 </div>
-                <svg
-                    width="40"
-                    height="40"
-                    viewBox="0 0 40 40"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <rect width="40" height="40" rx="20" fill="#161616" />
-                    <path d="M25 20H15" stroke="white" strokeLinecap="round" />
-                    <path d="M20 25V15" stroke="white" strokeLinecap="round" />
-                </svg>
+                <button className="header__button">
+                    <svg
+                        width="40"
+                        height="40"
+                        viewBox="0 0 40 40"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <rect width="40" height="40" rx="20" fill="#161616" />
+                        <path
+                            d="M25 20H15"
+                            stroke="white"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M20 25V15"
+                            stroke="white"
+                            strokeLinecap="round"
+                        />
+                    </svg>
+                </button>
             </div>
         </header>
     );
